7f239012a92fe68b588b015169a190644115e483
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-11-27 14:34:42 -0500

    remove unused import rename 'txid' parameter to 'tx_id'. using PEP 8 single_trailing_underscore_ convention

--- src/fabric_shim/stub.py
+++ src/fabric_shim/stub.py
@@ -3,11 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 from src.fabric_shim.interfaces import ChaincodeStubInterface
 from src.fabric_shim.utils import *
-from fabric_protos_python.peer import chaincode_pb2 as pb
 from fabric_protos_python.common import common_pb2 as cm_pb
 from fabric_protos_python.peer import proposal_pb2 as pr_pb
 from fabric_protos_python.msp import identities_pb2 as id_pb
-from fabric_protos_python.peer import chaincode_event_pb2 as e_pb
 from collections.abc import Sequence
 from src.fabric_shim.logging import LOGGER
 
@@ -17,10 +15,10 @@ VALIDATION_PARAMETER: str = 'VALIDATION_PARAMETER'
 class ChaincodeStub(ChaincodeStubInterface):
     """The stub encapsulates the APIs between the chaincode implementation and the Fabric peer"""
 
-    def __init__(self, client, channel_id, txid, cc_input, signed_proposal_pb):
+    def __init__(self, client, channel_id, tx_id, cc_input, signed_proposal_pb):
         self.client = client
         self.channel_id = channel_id
-        self.txid = txid
+        self.tx_id = tx_id
         self.cc_input = cc_input
         self.signed_proposal_pb = signed_proposal_pb
         self.validationParameterMetakey = VALIDATION_PARAMETER
@@ -88,14 +86,14 @@ class ChaincodeStub(ChaincodeStubInterface):
 
     def get_txid(self):
         """Get the ID of the chaincode calling transaction"""
-        return self.txid
+        return self.tx_id
 
     async def get_state(self, key: str) -> bytearray:
         """Get asset state from ledger"""
         LOGGER.info('get_state called with key:%s' % key)
         # Access public data by setting the collection to empty string
         collection = ''
-        return await self.client.handle_get_state(collection, key, self.channel_id, self.txid)
+        return await self.client.handle_get_state(collection, key, self.channel_id, self.tx_id)
 
     async def put_state(self, key: str, value):
         """Put asset state to ledger"""
@@ -104,14 +102,14 @@ class ChaincodeStub(ChaincodeStubInterface):
         collection = ''
         if isinstance(value, str):
             value = bytes(value.encode())
-        return await self.client.handle_put_state(collection, key, value, self.channel_id, self.txid)
+        return await self.client.handle_put_state(collection, key, value, self.channel_id, self.tx_id)
 
     async def delete_state(self, key: str):
         """Delete asset state from ledger"""
         LOGGER.info('delete_state called with key:%s' % key)
         # Access public data by setting the collection to empty string
         collection = ''
-        return await self.client.handle_delete_state(collection, key, self.channel_id, self.txid)
+        return await self.client.handle_delete_state(collection, key, self.channel_id, self.tx_id)
 
     def create_composite_key(self, object_type, attributes):
         """Creates a composite key by combining the objectType string
